2e9d496ff1975070e287b0577446ef7f62a5d713
rtv22 <rtvcojigor@gmail.com>
2017-09-26 22:22:41 +0300

    added sources

---
+++ include/print.hpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+void print(const std::string& text, std::ostream& out = std::cout);
+void print(const std::string& text, std::ofstream& out);
+EOF
+
+EOF
+eof
+$cat > examples/example1.cpp <<EOF
+<< EOF
+>>EOF
+EOF
+ls
+:!q
+!q
