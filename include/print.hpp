d1dd18e169073efd3bb787fdd46e38abdb453886
rtv22 <rtvcojigor@gmail.com>
2017-10-02 22:09:18 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -4,14 +4,3 @@
 
 void print(const std::string& text, std::ostream& out = std::cout);
 void print(const std::string& text, std::ofstream& out);
-EOF
-
-EOF
-eof
-$cat > examples/example1.cpp <<EOF
-<< EOF
->>EOF
-EOF
-ls
-:!q
-!q
